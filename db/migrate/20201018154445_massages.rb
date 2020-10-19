c37f9647a5f83f23b33a5341d0231120c5db7c6f
haruharu <haruka373000@gmail.com>
2020-10-19 14:26:03 +0900

    チャットルームの削除機能の実装

--- db/migrate/20201018154445_massages.rb
+++
@@ -1,5 +0,0 @@
-class Massages < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :massages
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_154445) do
+ActiveRecord::Schema.define(version: 2020_10_18_152526) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
